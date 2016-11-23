2ef6a8055ac64b1c5792f8e331b0d0c38140eef2
yannmango <yananchen@v1020-wn-129-115.campus-dynamic.uwaterloo.ca>
2016-11-22 20:00:42 -0500

    add "change profile image" feature
    fix bug: toolbar will be invisible when changing theme
    add feature: change profile image

--- app/src/main/java/ca/uwaterloo/camevent/MainActivity.java
+++ app/src/main/java/ca/uwaterloo/camevent/MainActivity.java
@@ -3,8 +3,12 @@ package ca.uwaterloo.camevent;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
@@ -24,6 +28,14 @@ import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class  MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener, DialogInterface.OnClickListener {
 
     private static final String TAG = "MainActivity";
@@ -32,6 +44,11 @@ public class  MainActivity extends BaseActivity implements NavigationView.OnNavi
     public final static int Red_Theme  = 0;
     public final static int Brown_Theme  = 1;
     public final static int BlueGrey_Theme=2;
+    public final static int Default_Theme=3;
+    private CircleImageView imageView;
+    private static final int REQUEST_CAMERA = 1;
+    private static final int SELECT_FILE = 2;
+    private static String userChoosenTask;
 
     int position;
 
@@ -58,7 +75,9 @@ public class  MainActivity extends BaseActivity implements NavigationView.OnNavi
             case BlueGrey_Theme:
                 setTheme(R.style.BlueGreyTheme);
                 break;
-            default:
+            case Default_Theme:
+                setTheme(R.style.AppTheme);
+
         }
         super.onCreate(savedInstanceState);
 
@@ -107,9 +126,17 @@ public class  MainActivity extends BaseActivity implements NavigationView.OnNavi
 
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerView=navigationView.getHeaderView(0);
+        imageView=(CircleImageView) headerView.findViewById(R.id.profile_image);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
 
         //How to change elements in the header programatically
-        View headerView = navigationView.getHeaderView(0);
+
         TextView emailText = (TextView) headerView.findViewById(R.id.email);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -134,7 +161,7 @@ public class  MainActivity extends BaseActivity implements NavigationView.OnNavi
     private void createDialog()
     {
         /** Options for user to select*/
-        String choose[] = {"Red_Theme","Brown_Theme","BlueGrey_Theme"};
+        String choose[] = {"Red_Theme","Brown_Theme","BlueGrey_Theme","Default_Theme"};
 
         AlertDialog.Builder b = new AlertDialog.Builder(this);
 
@@ -214,6 +241,127 @@ public class  MainActivity extends BaseActivity implements NavigationView.OnNavi
         getMenuInflater().inflate(R.menu.menu_me, menu);
 
         return true;
+    }
+    private void selectImage() {
+        final CharSequence[] items = { "Take Photo", "Choose from Library",
+                "Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                boolean result=Utility.checkPermission(MainActivity.this);
+                if (items[item].equals("Take Photo")) {
+                    userChoosenTask="Take Photo";
+
+                    if(result)
+                        cameraIntent();
+                } else if (items[item].equals("Choose from Library")) {
+                    userChoosenTask="Choose from Library";
+                    if(result)
+                        galleryIntent();
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+
+    }
+
+    private void galleryIntent() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);//
+        startActivityForResult(Intent.createChooser(intent, "Select File"),SELECT_FILE);
+    }
+    //- See more at: http://www.theappguruz.com/blog/android-take-photo-camera-gallery-code-sample#sthash.3i3KjY1Q.dpuf
+
+
+    private void cameraIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case Utility.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if(userChoosenTask.equals("Take Photo"))
+                        cameraIntent();
+                    else if(userChoosenTask.equals("Choose from Library"))
+                        galleryIntent();
+                } else {
+//code for deny
+                }
+                break;
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CAMERA)
+            {
+                Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+                File destination = new File(Environment.getExternalStorageDirectory(),
+                        System.currentTimeMillis() + ".jpg");
+
+                FileOutputStream fo;
+                try {
+                    destination.createNewFile();
+                    fo = new FileOutputStream(destination);
+                    fo.write(bytes.toByteArray());
+                    fo.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                imageView.setImageBitmap(thumbnail);
+
+
+            }
+            //onSelectFromGalleryResult(data);
+            else if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+        }
+    }
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+        Bitmap bm=null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        imageView.setImageBitmap(bm);
+    }
+    //- See more at: http://www.theappguruz.com/blog/android-take-photo-camera-gallery-code-sample#sthash.3i3KjY1Q.dpuf
+
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        imageView.setImageBitmap(thumbnail);
     }
 
 
